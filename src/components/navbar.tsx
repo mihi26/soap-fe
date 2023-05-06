40235ab8e9a7a931bbc578f8dd7c66dfbd3c3807
MiHi <minhhieuu26@gmail.com>
2023-05-07 00:14:14 +0700

    [front]: delete upper navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
       <div className="container px-1">
         <a className="navbar-brand font-weight-bolder ms-lg-0 " href="/home">
-          Astro Ecommerce
+          TakaTiki Ecommerce
         </a>
         <button
           className="navbar-toggler shadow-none ms-2"
--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,42 +1,40 @@
-import React from 'react'
-import data from '../mock/data.json';
-import '../assets/scss/soap-ecommerce.scss';
-import Navbar from '../components/navbar';
-import UpperNavbar from '../components/store/upperNavbar';
-import CardProduct from '../components/products/cardProduct';
-import ShoppingCart from '../components/cart/shoppingCart';
-import StoreDoubleColumn from '../components/store/storeDoubleColumn';
+import React from "react";
+import data from "../mock/data.json";
+import "../assets/scss/soap-ecommerce.scss";
+import Navbar from "../components/navbar";
+import CardProduct from "../components/products/cardProduct";
+import ShoppingCart from "../components/cart/shoppingCart";
+import StoreDoubleColumn from "../components/store/storeDoubleColumn";
 export function CheckoutPage() {
   let cartItems = [];
-data.shoppingCart.map(id => 
-  data.products.filter(x => x.id == id).map(x => cartItems.push(x))
-)
+  data.shoppingCart.map((id) =>
+    data.products.filter((x) => x.id == id).map((x) => cartItems.push(x))
+  );
   return (
     <React.Fragment>
       <main>
-    <UpperNavbar />
-    <Navbar />
-    <ShoppingCart products = {cartItems}/>
-    <div class="container mt-5">
-      <div class="row">
-        <h5 class="mb-4">You may also like...</h5>
-        {data.products.map(product => 
-          <div class="col-md-6 col-lg-3">
-            <CardProduct 
-              thumb_src = {product.thumb_src}
-              thumb_alt = {product.thumb_alt}
-              color = {product.color}
-              title = {product.title}
-              price = {product.price}
-              position = "left"
-            />
+        <Navbar />
+        <ShoppingCart products={cartItems} />
+        <div class="container mt-5">
+          <div class="row">
+            <h5 class="mb-4">You may also like...</h5>
+            {data.products.map((product) => (
+              <div class="col-md-6 col-lg-3">
+                <CardProduct
+                  thumb_src={product.thumb_src}
+                  thumb_alt={product.thumb_alt}
+                  color={product.color}
+                  title={product.title}
+                  price={product.price}
+                  position="left"
+                />
+              </div>
+            ))}
           </div>
-        )}        
-      </div>
-      <hr class="dark horizontal my-5" />
-      <StoreDoubleColumn />
-    </div>
-  </main>
+          <hr class="dark horizontal my-5" />
+          <StoreDoubleColumn />
+        </div>
+      </main>
     </React.Fragment>
-  )
+  );
 }
