2cea047e7502b00a5935a02379bd000bb32f2702
MiHi <minhhieuu26@gmail.com>
2023-05-17 16:08:43 +0700

    remove navbar on login

--- src/pages/Auth/AuthPage.jsx
+++ src/pages/Auth/AuthPage.jsx
@@ -6,62 +6,6 @@ import logo from "../../assets/img/logo.png";
 export const AuthPage = () => {
   return (
     <React.Fragment>
-      <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0 navbar-z">
-      <div className="container px-1" style={{ maxHeight: "50px" }}>
-        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="/product">
-          <img height={50} src={logo} alt="" style={{ scale: "1.5" }} />
-        </a>
-        <button
-          className="navbar-toggler shadow-none ms-2"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navigation"
-          aria-controls="navigation"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon mt-2">
-            <span className="navbar-toggler-bar bar1"></span>
-            <span className="navbar-toggler-bar bar2"></span>
-            <span className="navbar-toggler-bar bar3"></span>
-          </span>
-        </button>
-        <div className="collapse navbar-collapse" id="navigation">
-          <ul className="navbar-nav ms-auto">
-            {(
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link text-dark dropdown-toggle font-weight-bold d-flex align-items-center me-2 "
-                  aria-current="categories"
-                  id="pagesExample"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Menu
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="pagesExample">
-                  <li>
-                    <a className="dropdown-item" href="/auth-page">
-                      Login
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/landing">
-                      Landing Page
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/product/">
-                      Product Page
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            )}
-          </ul>
-        </div>
-      </div>
-    </nav>
       <main className="auth-main">
         <div className="authPage">
           <div
